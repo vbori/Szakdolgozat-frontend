d92f25f79b737466e19f08b9a92010fda2f2c7c3
Varga Bori <vx53gk@inf.elte.hu>
2023-10-27 13:38:02 +0200

    Saving before windows reset

--- src/app/participant-view/welcome-page/welcome-page.component.ts
+++ src/app/participant-view/welcome-page/welcome-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,5 +7,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent {
+  participantId: FormControl = new FormControl('', [Validators.required]);
   @Output() nextStep = new EventEmitter();
+
+  checkId() {
+    console.log(this.participantId.value);
+    this.nextStep.emit();
+  }
 }
